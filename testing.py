21ee4990083f6b7947744cfb4185472a73039feb
dapinkone <crystalshade@gmail.com>
2016-02-20 01:56:19 -0500

    added FPS and Score text.(inefficient, but working). Text is slightly different than regular sprites. TODO: figure out a way to .update() them by group?

--- testing.py
+++ testing.py
@@ -1,8 +1,5 @@
 # just getting started with pygame.
-# several bugs/dependancy issues solved by: python 3, compiling pygame 1.9.2 from source
-# and apt-get install python3-dev python-dev libsdl-image1.2-dev libsdl-mixer1.2-dev
-# libsdl-ttf2.0-dev libsdl1.2-dev libsmpeg-dev python3-numpy
-# python-numpy subversion libportmidi-dev libfreetype6-dev
+# Dependancies removed -- see ./Dependancies
 #
 # TODO:
 # randomly generate enemies
@@ -11,14 +8,16 @@
 # animations or something upon death/explosion of a ship.
 # score displayed bottom left
 
-import pygame, sys
-#import os.path
-# sys.path.append(os.path.dirname("__file__")) # add the program root dir to path.
+import pygame
+import sys
+import random
+# import os.path
 # from pygame.locals import * # sloppy. We can do better.
 
 # pygame.init mysteriously crashes on debian when pygame.quit is called.
 # lets use pygame.display instead, and hope that doesn't cause issues.
 pygame.display.init()
+pygame.font.init()  # it caused issues.
 
 fpsClock = pygame.time.Clock()  # allow limiting FPS
 
@@ -34,10 +33,13 @@ score = 0
 
 # set_mode(width, height) making the window
 screen = pygame.display.set_mode((640, 640))
+screen_width  = screen.get_width()
+screen_height = screen.get_height()
+
 pygame.display.set_caption('hello pygame :D')
 
 
-class Sprite(pygame.sprite.Sprite):
+class S_Picture(pygame.sprite.Sprite):
 
     def __init__(self, image_filename, x, y):
         pygame.sprite.Sprite.__init__(self)
@@ -45,9 +47,9 @@ class Sprite(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = x, y
 
-class Enemy(Sprite):
+class Enemy(S_Picture):
     def __init__(self, x, y):
-        Sprite.__init__(self, self.image, x, y)
+        S_Picture.__init__(self, self.image, x, y)
     image = './assets/Enemy_ship.png'
 
     def update(self):
@@ -56,26 +58,37 @@ class Enemy(Sprite):
             self.rect.y = 20
 
 
-class Player(Sprite):
+class Player(S_Picture):
     def __init__(self, x, y):
-        Sprite.__init__(self, self.image, x, y)
+        S_Picture.__init__(self, self.image, x, y)
     image = './assets/SpaceShip.png'
 
-all_sprites_list = pygame.sprite.Group()
-player_sprite = Player(300, 500) # default player position.
-all_sprites_list.add(player_sprite)
+class Text(pygame.sprite.Sprite):
+    def __init__(self, text, size=16, color=white_color, width=40, height=40):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.font = pygame.font.SysFont("Arial", size)
+        self.textSurf = self.font.render(text, 1, color)
+
+player_sprite = Player(300, 500)  # default player ship pos/sprite
+
+all_sprites_list = pygame.sprite.Group(player_sprite)
+
+# generate some enemies
 enemy_group = pygame.sprite.Group()
 for x in range(8):
     print("enemy {}".format(x))
-    enemy = Enemy(100*x, 0)
+    enemy = Enemy(100 * x, 0)
     enemy_group.add(enemy)
 all_sprites_list.add(enemy_group)
+
 
 # the mouse appears to lag a little bit, but cursor still gets action.
 # solution? no more mouse. still lags, but less game-breaking.
 pygame.mouse.set_visible(False)
 
 done = False
+
 while not done:  # main loop
     for event in pygame.event.get():
         if event.type in (pygame.QUIT, pygame.K_ESCAPE):
@@ -92,21 +105,27 @@ while not done:  # main loop
             done = True
             break
 
-    screen.fill(black_color)  # clean screen
-
-    # paint our stuff to the screen.
-    # screen.blit(ship_sprite.image, ship_sprite.rect)
-    # pygame.draw.circle(screen, white_color, (cursor_x, cursor_y), 10, 10)
-
     # now checking for collision detection. not perfect, but enough for now.
-    for sprite in pygame.sprite.spritecollide(player_sprite, \
+    for sprite in pygame.sprite.spritecollide(player_sprite,
                                               enemy_group, True):
         print("BOOM {} {}".format(score, sprite))
         score = score + 1
-    all_sprites_list.draw(screen)
+
+    # clear the screen
+    screen.fill(black_color)
+
+    # update all the things!
     all_sprites_list.update()
+
+    # draw all the things!
+    all_sprites_list.draw(screen)
+    screen.blit(Text("Score: {}".format(score)).textSurf, (20, 5))
+    FPS_text = Text("FPS: {:.4}".format(fpsClock.get_fps())).textSurf
+    screen.blit(FPS_text, (screen_width - FPS_text.get_width() - 20, 5))
     pygame.display.flip()  # reveal changes
-    print(fpsClock.tick(60))  # we don't need more than 60 fps for this. srsly.
+
+    # slow it down, if necessary.
+    fpsClock.tick(60)  # we don't need more than 60 fps for this. srsly.
 print("mainloop finished")
 pygame.quit()
-# sys.exit()
+sys.exit()
