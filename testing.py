a78e0c553c226f0080f06a7f3f182c48d5f3b26f
dapinkone <crystalshade@gmail.com>
2016-02-19 21:05:05 -0500

    success! added working rough colision detection, enemy/player spawn in reasonble locations, and fixed transparency issues I had previously. notes on dependency issues included." "

--- testing.py
+++ testing.py
@@ -1,4 +1,9 @@
 # just getting started with pygame.
+# several bugs/dependancy issues solved by: python 3, compiling pygame 1.9.2 from source
+# and apt-get install python3-dev python-dev libsdl-image1.2-dev libsdl-mixer1.2-dev
+# libsdl-ttf2.0-dev libsdl1.2-dev libsmpeg-dev python3-numpy
+# python-numpy subversion libportmidi-dev libfreetype6-dev
+#
 # TODO:
 # enemies
 # weapons fire
@@ -12,8 +17,8 @@ import os.path
 # sys.path.append(os.path.dirname("__file__")) # add the program root dir to path.
 # from pygame.locals import * # sloppy. We can do better.
 
-# pygame.init crashes on debian when pygame.quit is called.
-# lets use pygame.display instead. hope that doesn't cause issues.
+# pygame.init mysteriously crashes on debian when pygame.quit is called.
+# lets use pygame.display instead, and hope that doesn't cause issues.
 pygame.display.init()
 
 fpsClock = pygame.time.Clock()  # allow limiting FPS
@@ -27,31 +32,46 @@ black_color = pygame.Color(0, 0, 0)
 # initialize variables
 mouse_x, mouse_y = 0, 0
 cursor_x, cursor_y = 0, 0
-
+score = 0
 
 # set_mode(width, height) making the window
 screen = pygame.display.set_mode((640, 640))
 pygame.display.set_caption('hello pygame :D')
 
-# sprites
-ship_img = './assets/SpaceShip.bmp'
-enemy_ship_img = './assets/Enemy_ship.png'
-
 
 class Sprite(pygame.sprite.Sprite):
 
-    def __init__(self, image_filename, width, height):
+    def __init__(self, image_filename, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([width, height])
         self.image = pygame.image.load(image_filename).convert_alpha()
         self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, y
 
-ship_sprite = Sprite(ship_img, 20, 20)
-enemy_ship_sprite = Sprite(enemy_ship_img, 30, 30)
+class Enemy(Sprite):
+    def __init__(self, x, y):
+        Sprite.__init__(self, self.image, x, y)
+    image = './assets/Enemy_ship.png'
 
-# default locations:
-ship_sprite.rect.topleft = [300, 500]
-enemy_ship_sprite.rect.topleft = [300, 10]
+    def update(self):
+        super(self).update()
+        self.rect.y = self.rect.y + 1
+        if self.rect.y > 600:
+            self.rect.y = 20
+
+class Player(Sprite):
+    def __init__(self, x, y):
+        Sprite.__init__(self, self.image, x, y)
+    image = './assets/SpaceShip.png'
+
+all_sprites_list = pygame.sprite.Group()
+ship_sprite = Player(300, 500) # default player position.
+all_sprites_list.add(ship_sprite)
+enemy_ships_group = pygame.sprite.Group()
+for x in range(4):
+    print("enemy {}".format(x))
+    enemy = Enemy(100*x, 0)
+    enemy_ships_group.add(enemy)
+    all_sprites_list.add(enemy)
 
 # the mouse appears to lag a little bit, but cursor still gets action.
 # solution? no more mouse. still lags, but less game-breaking.
@@ -63,8 +83,9 @@ while main_loop:  # main loop
         if event.type in (pygame.QUIT, pygame.K_ESCAPE):
             main_loop = False
         elif event.type == pygame.MOUSEMOTION:
-            mouse_x, mouse_y = event.pos
-            ship_sprite.rect.topleft = [mouse_x, mouse_y]
+            x, y = pygame.mouse.get_pos()
+            ship_sprite.rect.x = x
+            ship_sprite.rect.y = y
 
         elif event.type == pygame.MOUSEBUTTONUP:
             mouse_x, mouse_y = event.pos
@@ -75,17 +96,18 @@ while main_loop:  # main loop
     screen.fill(blue_color)  # clean screen
 
     # paint our stuff to the screen.
-    screen.blit(ship_sprite.image, ship_sprite.rect)
+    # screen.blit(ship_sprite.image, ship_sprite.rect)
     # pygame.draw.circle(screen, white_color, (cursor_x, cursor_y), 10, 10)
-    screen.blit(enemy_ship_sprite.image, enemy_ship_sprite.rect)  # temp coordinates for testing.
 
-    # now checking for collision detection
-    #for sprite in pygame.sprite.spritecollide(ship_sprite, enemy_ship_sprite, True):
-    #    print("BOOM")
-
+    # now checking for collision detection. not perfect, but enough for now.
+    for sprite in pygame.sprite.spritecollide(ship_sprite, \
+                                              enemy_ships_group, True):
+        print("BOOM {} {}".format(score, sprite))
+        score = score + 1
+    all_sprites_list.draw(screen)
     pygame.display.flip()  # reveal changes
 
     fpsClock.tick(60)  # we don't need more than 60 fps for this. srsly.
 print("mainloop finished")
 pygame.quit()
-sys.exit()
+# sys.exit()
